cb5dbe5588ec4380bd8141ec922c6ad09f736ef4
Ksawery Meister <ksawerymeister@gmail.com>
2020-05-26 21:54:52 +0200

    Book Runner

---
+++ kodilla-intro/src/main/java/BookRunner.java
@@ -0,0 +1,11 @@
+public class BookRunner {
+
+    public static void main(String[] args) {
+
+        Book book = Book.of("Isaac Asimov", "The Galaxy");
+
+        System.out.println(book);
+
+    }
+
+}
